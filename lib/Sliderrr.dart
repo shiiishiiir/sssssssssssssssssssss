289d2fbc3778fc0fde0bcf2ca29183e10b0515b7
shishir <shiiiishiiiir@gmail.com>
2021-11-07 14:07:19 +0600

    slider carousel sliver

--- lib/Sliderrr.dart
+++ lib/Sliderrr.dart
@@ -9,50 +9,47 @@ class sliderrr extends StatefulWidget {
 }
 
 class _sliderrrState extends State<sliderrr> {
- var images= [
-   "https://www.xda-developers.com/files/2018/02/Flutter-Framework-Feature-Image-Background-Colour-810x298_c.png",
-   "https://flutter.dev/assets/images/showcase/cards/bmw-top-1200.jpg",
-   "https://flutter.dev/assets/images/showcase/cards/nubank-top-600.jpg"
- ];
+  var images = [
+    "https://www.xda-developers.com/files/2018/02/Flutter-Framework-Feature-Image-Background-Colour-810x298_c.png",
+    "https://flutter.dev/assets/images/showcase/cards/bmw-top-1200.jpg",
+    "https://flutter.dev/assets/images/showcase/cards/nubank-top-600.jpg"
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Container(
-            height: 300,
-            child: CarouselSlider(
-              options: CarouselOptions(
-                height: 400,
-                aspectRatio: 16/9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                reverse: false,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enlargeCenterPage: true,
-                // onPageChanged: callbackFunction,
-                scrollDirection: Axis.horizontal,),
-              items: images.map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                            // color: Colors.amber,
-                        ),
-                        child: Image.network(i),
-                    );
-                  },
-                );
-              }).toList(),
+        body: Container(
+          height: 300,
+          child: CarouselSlider(
+            options: CarouselOptions(
+              height: 400,
+              aspectRatio: 16 / 9,
+              viewportFraction: 0.8,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              // onPageChanged: callbackFunction,
+              scrollDirection: Axis.horizontal,
             ),
-
+            items: images.map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(
+                        // color: Colors.amber,
+                        ),
+                    child: Image.network(i),
+                  );
+                },
+              );
+            }).toList(),
           ),
         ),
       ),
--- lib/dropsliver.dart
+++ lib/dropsliver.dart
@@ -85,3 +85,68 @@ class bottomsheett extends StatelessWidget {
     );
   }
 }
+
+
+
+
+class sliverrrr extends StatefulWidget {
+  const sliverrrr({Key? key}) : super(key: key);
+
+  @override
+  _sliverrrrState createState() => _sliverrrrState();
+}
+
+class _sliverrrrState extends State<sliverrrr> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 200,
+              backgroundColor: Colors.cyan,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text("Sliver App Bar fixed"),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                <Widget>[
+                  addDetails("name", "description"),
+                  addDetails("one", "description"),
+                  addDetails("two", "description"),
+                  addDetails("three", "description"),
+                  addDetails("four", "description"),
+                  addDetails("five", "description"),
+                  addDetails("six", "description"),
+                  addDetails("seven", "description"),
+                  addDetails("eight", "description"),
+                  addDetails("nine", "description"),
+                  addDetails("ten", "description"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+Widget addDetails(
+    String name,
+    String description,
+    ) {
+  return ListTile(
+    title: Text(name),
+    subtitle: Text(description),
+    leading: CircleAvatar(
+      child: Text(name[0]),
+    ),
+  );
+}
+
