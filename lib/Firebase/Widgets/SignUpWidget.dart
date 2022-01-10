8011dc63733b7785fd50dcb2fbb01f12ecafdb3e
shishir <shiiiishiiiir@gmail.com>
2022-01-11 02:00:34 +0600

    Google Auth Firebase Email Auth

--- lib/Firebase/Widgets/SignUpWidget.dart
+++ lib/Firebase/Widgets/SignUpWidget.dart
@@ -14,22 +14,22 @@ class SignUpWidget extends StatelessWidget {
 
   Widget buildSignUp() => Column(
         children: [
-          // Spacer(),
-          // Align(
-          //   alignment: Alignment.centerLeft,
-          //   child: Container(
-          //     margin: EdgeInsets.symmetric(horizontal: 20),
-          //     width: 175,
-          //     child: Text(
-          //       'Welcome Back To MyApp',
-          //       style: TextStyle(
-          //         color: Colors.black54,
-          //         fontSize: 30,
-          //         fontWeight: FontWeight.bold,
-          //       ),
-          //     ),
-          //   ),
-          // ),
+          Spacer(),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              width: 175,
+              child: Text(
+                'Welcome Back To MyApp',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
           Spacer(),
           GoogleSignUpButton(),
           SizedBox(height: 12),
